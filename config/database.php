79850035bfb4b883373da4afad20183c2592f86e
Cherry <lenyamm35@gmail.com>
2024-01-09 23:39:05 +0200

    Database connected

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'database' => 'php-mvc',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
